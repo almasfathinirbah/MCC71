f53aaf0813d52fa57c594402d4d4b84d3fe4570d
almasfathinirbah <85482667+almasfathinirbah@users.noreply.github.com>
2022-11-03 17:04:40 +0700

    update tugas API

---
+++ API/Repositories/Data/AccountRepository.cs
@@ -0,0 +1,42 @@
+﻿using API.Context;
+using API.ViewModels;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Repositories.Data
+{
+    public class AccountRepository
+    {
+        private readonly MyContext myContext;
+
+        public AccountRepository(MyContext context)
+        {
+            myContext = context;
+        }
+
+        public ResponseLogin Login()
+        {
+            if (email != null)
+            {
+                var data = myContext.Users
+                .Include(x => x.Employee)
+                .Include(x => x.Role)
+                .SingleOrDefault(x => x.Employee.Email.Equals(email));
+                var result = Hashing.ValidatePassword(password, data.Password);
+                if (result == true)
+                {
+                    HttpContext.Session.SetInt32("Id", data.Id);
+                    HttpContext.Session.SetString("FullName", data.Employee.FullName);
+                    HttpContext.Session.SetString("Email", data.Employee.Email);
+                    HttpContext.Session.SetString("Role", data.Role.Name);
+
+                    return 1;
+                }
+                else
+                {
+                    return 2;
+                }
+            }
+            return 3;
+        }
+    }
+}
---
+++ API/ViewModels/ResponseLogin.cs
@@ -0,0 +1,10 @@
+﻿namespace API.ViewModels
+{
+    public class ResponseLogin
+    {
+        public string Id { get; set; }
+        public string FullName { get; set; }
+        public string Role { get; set; }
+        public string Email { get; set; }
+    }
+}
